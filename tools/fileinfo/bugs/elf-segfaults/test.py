e3c2804cbd2775eb032756be47fa38eb7ee3847f
Michal Bandzi <m.bandzi@gmail.com>
2018-03-03 15:35:18 +0100

    Add links to github issues for some tests

--- tools/fileinfo/bugs/elf-segfaults/test.py
+++ tools/fileinfo/bugs/elf-segfaults/test.py
@@ -1,8 +1,8 @@
 from regression_tests import *
 
 
+# https://github.com/avast-tl/retdec/issues/89
 class TestStringTableSegfault(Test):
-
     settings = TestSettings(
         tool='fileinfo',
         input='79e34ba714e38c7e72f30b5dcb54ba4e4e05915dbaece7fdf4271b1410306e04'
@@ -12,8 +12,8 @@ class TestStringTableSegfault(Test):
         assert self.fileinfo.succeeded
 
 
+# https://github.com/avast-tl/retdec/issues/89
 class TestDynamicSegfault(Test):
-
     settings = TestSettings(
         tool='fileinfo',
         input='4dfc8ebfa6d90d2fbacf56c035b04aa38a3c595f462904f4a038d45dcb9d3a7b'
