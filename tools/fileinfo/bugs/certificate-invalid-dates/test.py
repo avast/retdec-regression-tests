28a95146d67cdf726646d1c6bd64b317b040587f
Marek Milkovič <marek.milkovic@avast.com>
2018-03-20 18:43:55 +0100

    tools/fileinfo: Add a test for avast-tl/retdec#251

---
+++ tools/fileinfo/bugs/certificate-invalid-dates/test.py
@@ -0,0 +1,13 @@
+from regression_tests import *
+
+class Test(Test):
+    settings=TestSettings(
+        tool='fileinfo',
+        args='--json --verbose',
+        input='03ea764ead2c27b0a98c48a18d4cc8e831f3d9a7bf62a471c2d57ef81183cf80'
+    )
+
+    def test_output_does_not_contain_any_date(self):
+        assert self.fileinfo.succeeded
+        assert 'validSince' not in self.fileinfo.output['certificateTable']['certificates'][0]['attributes']['subject']
+        assert 'validUntil' not in self.fileinfo.output['certificateTable']['certificates'][0]['attributes']['subject']
