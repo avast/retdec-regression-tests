e26ffe48c40f7b0e37b1dea73cd7ccf189c7d81e
Marek Milkovič <marek.milkovic@avast.com>
2018-03-21 15:24:55 +0100

    tools/fileinfo: Added test for avast-tl/retdec#255

--- tools/fileinfo/bugs/certificate-segfault/test.py
+++ tools/fileinfo/bugs/certificate-segfault/test.py
@@ -27,3 +27,19 @@ class Test2(Test):
 		assert 'signerCertificateIndex' not in self.fileinfo.output['certificateTable']
 		self.assertEqual(self.fileinfo.output['certificateTable']['numberOfCertificates'], '1')
 		self.assertEqual(self.fileinfo.output['certificateTable']['certificates'][0]['sha256'], 'CED5AB020125966499A067ABFB138434281BC5B00C90D5D74D31529FF5169BF2')
+
+
+# https://github.com/avast-tl/retdec/issues/255
+class Test3(Test):
+	settings=TestSettings(
+		tool='fileinfo',
+		args='--json --verbose',
+		input=[
+			'65BB4991028F627C5B832C5E9189118FF656F71481931BB98EB9F211FA4F6B5F',
+			'DA9AC36A6A69CBE79D266AE695906296F8AABDE7713624D29553BC22508D9AC3'
+		]
+	)
+
+	def test_certificate_table_present(self):
+		assert self.fileinfo.succeeded
+		self.assertEqual(self.fileinfo.output['certificateTable']['certificates'][0]['sha256'], 'A2219C3E44EE3748EAE12E5AA6C961AF47C185E25A8E59AFFD8FCAED641286CD')
