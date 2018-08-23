93f9d667a0f9c01d0ab1dd5988964d460f80a5bd
Peter Matula <peter.matula@avast.com>
2018-08-23 13:17:17 +0200

    disable 2 tests on macos
    They are timeouting after 1200 seconds. It looks like there is some nondeterminism going on, because one of them (my-printf-decompilation-quality) did successfully run (once) when run manually on macOS server. There are slight differences between *.ll produces on macOS server and my Linux. Related to https://github.com/avast-tl/retdec/issues/209.

--- bugs/my-printf-decompilation-quality/test.py
+++ bugs/my-printf-decompilation-quality/test.py
@@ -1,20 +1,21 @@
 
 from regression_tests import *
 
-class Test(Test):
-	settings = TestSettings(
-		input='x86-c96c707952ce5bf3334f97c3e76afba6'
-	)
+if not on_macos():
+    class Test(Test):
+        settings = TestSettings(
+            input='x86-c96c707952ce5bf3334f97c3e76afba6'
+        )
 
-	def test_my_printf_calls_known_functions(self):
-		pass
-		# TODO Currently, decfront generates my_printf(), but it is not
-		#      reachable from main(), so it is removed and the test fails. See
-		#      #1066 for more details.
-		# self.out_c.has_funcs('my_printf')
-		# fnc = self.out_c.funcs['my_printf']
-		# assert fnc.calls('write')
-		# assert fnc.calls('strlen')
-		# assert fnc.calls('strchr')
-		# assert fnc.calls('memcpy')
-		# assert fnc.calls('memset')
+        def test_my_printf_calls_known_functions(self):
+            pass
+            # TODO Currently, decfront generates my_printf(), but it is not
+            #      reachable from main(), so it is removed and the test fails. See
+            #      #1066 for more details.
+            # self.out_c.has_funcs('my_printf')
+            # fnc = self.out_c.funcs['my_printf']
+            # assert fnc.calls('write')
+            # assert fnc.calls('strlen')
+            # assert fnc.calls('strchr')
+            # assert fnc.calls('memcpy')
+            # assert fnc.calls('memset')
