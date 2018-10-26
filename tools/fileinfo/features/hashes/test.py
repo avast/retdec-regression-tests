7c15d2febcb4aa3ef4d847a40e4af6fa23d07cd6
Jakub Pruzinec <j.pruzinec@gmail.com>
2018-10-26 20:06:04 +0200

    typeref with nested classes and infinite loop referencing

--- tools/fileinfo/features/hashes/test.py
+++ tools/fileinfo/features/hashes/test.py
@@ -109,10 +109,11 @@ class TestExportHashMACHO(Test):
 
 # https://github.com/avast-tl/retdec/issues/363
 # Test typeref hashes for .NET files
-class TestTypeRefHash(Test):
+# Default VS binary
+class TestTypeRefHashDefault(Test):
     settings = TestSettings(
         tool='fileinfo',
-        input='typeref_hash',
+        input='typeref_hash_default',
         args='--verbose --json'
     )
 
@@ -122,3 +123,33 @@ class TestTypeRefHash(Test):
         self.assertEqual(self.fileinfo.output['typeRefTable']['crc32'], 'bb390cc9')
         self.assertEqual(self.fileinfo.output['typeRefTable']['md5'], '93b7f964c87a94b07d1f6171f0b7d7c1')
         self.assertEqual(self.fileinfo.output['typeRefTable']['sha256'], '37a37a2d4cc651a9b6cf54da949cbb6c89fc1e6e9991628087741d51666c7f1b')
+
+# Custom VS nested class binary
+class TestTypeRefHashNested(Test):
+    settings = TestSettings(
+        tool='fileinfo',
+        input='typeref_hash_nested',
+        args='--verbose --json'
+    )
+
+    def test_correctly_computes_typeref_hash(self):
+        assert self.fileinfo.succeeded
+
+        self.assertEqual(self.fileinfo.output['typeRefTable']['crc32'], '2553516c')
+        self.assertEqual(self.fileinfo.output['typeRefTable']['md5'], '5742603226df6e720f055413ba924c2c')
+        self.assertEqual(self.fileinfo.output['typeRefTable']['sha256'], 'c40e879174d85aaf426217ab5e39de04214245636550b82051a759d2946f15db')
+
+# VS nested class binary with cyclic referencing
+class TestTypeRefHashNested(Test):
+    settings = TestSettings(
+        tool='fileinfo',
+        input='typeref_hash_nested_infinite',
+        args='--verbose --json'
+    )
+
+    def test_correctly_computes_typeref_hash(self):
+        assert self.fileinfo.succeeded
+
+        self.assertEqual(self.fileinfo.output['typeRefTable']['crc32'], '0c472e09')
+        self.assertEqual(self.fileinfo.output['typeRefTable']['md5'], 'f8a9907a02d8a1bede13aa8dfc005269')
+        self.assertEqual(self.fileinfo.output['typeRefTable']['sha256'], '9b327187b627a940bfe8645ca3df66500906f1822cfe08e279cbf23a7dd23660')
