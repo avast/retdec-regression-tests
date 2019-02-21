d4790bc9de9eda390f81f5c29be89a5eef6b53db
Peter Matula <peter.matula@avast.com>
2019-02-21 10:58:22 +0100

    https://github.com/avast-tl/retdec/issues/479: enable all disabled tests

--- bugs/my-printf-decompilation-quality/test.py
+++ bugs/my-printf-decompilation-quality/test.py
@@ -1,17 +1,16 @@
 
 from regression_tests import *
 
-if not on_macos():
-    class Test(Test):
-        settings = TestSettings(
-            input='x86-c96c707952ce5bf3334f97c3e76afba6'
-        )
+class Test(Test):
+    settings = TestSettings(
+        input='x86-c96c707952ce5bf3334f97c3e76afba6'
+    )
 
-        def test_my_printf_calls_known_functions(self):
-            self.out_c.has_funcs('my_printf')
-            fnc = self.out_c.funcs['my_printf']
-            assert fnc.calls('write')
-            assert fnc.calls('strlen')
-            assert fnc.calls('strchr')
-            assert fnc.calls('memcpy')
-            assert fnc.calls('memset')
+    def test_my_printf_calls_known_functions(self):
+        self.out_c.has_funcs('my_printf')
+        fnc = self.out_c.funcs['my_printf']
+        assert fnc.calls('write')
+        assert fnc.calls('strlen')
+        assert fnc.calls('strchr')
+        assert fnc.calls('memcpy')
+        assert fnc.calls('memset')
