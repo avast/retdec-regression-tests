5d68bde1454dc588c2f66d8246b63c5eb00f2944
Jakub Pruzinec <j.pruzinec@gmail.com>
2019-03-03 14:00:47 +0100

    normalization to 0-8

--- tools/fileinfo/features/entropy/test.py
+++ tools/fileinfo/features/entropy/test.py
@@ -10,8 +10,8 @@ class TestPE(Test):
 
     def test_section_entropy(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][0]['entropy'], '0.653098')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][2]['entropy'], '0.990941')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][0]['entropy'], '5.22479')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][2]['entropy'], '7.92753')
 
     def test_overlay_entropy(self):
         assert self.fileinfo.succeeded
@@ -26,38 +26,38 @@ class TestELF(Test):
 
     def test_section_entropy(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][1]['entropy'], '0.492595')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][2]['entropy'], '0.19516')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][3]['entropy'], '0.523951')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][4]['entropy'], '0.0614047')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][5]['entropy'], '0.0733867')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][6]['entropy'], '0.567219')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][7]['entropy'], '0.0936994')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][8]['entropy'], '0.223194')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][9]['entropy'], '0.173947')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][10]['entropy'], '0.122919')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][11]['entropy'], '0.529546')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][12]['entropy'], '0.381569')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][13]['entropy'], '0.683252')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][14]['entropy'], '0.404863')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][15]['entropy'], '0.426302')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][16]['entropy'], '0.389928')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][17]['entropy'], '0.555075')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][18]['entropy'], '0.13266')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][19]['entropy'], '0.13266')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][20]['entropy'], '0.176947')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][1]['entropy'], '3.94076')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][2]['entropy'], '1.56128')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][3]['entropy'], '4.19161')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][4]['entropy'], '0.491237')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][5]['entropy'], '0.587093')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][6]['entropy'], '4.53775')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][7]['entropy'], '0.749595')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][8]['entropy'], '1.78555')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][9]['entropy'], '1.39158')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][10]['entropy'], '0.983356')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][11]['entropy'], '4.23637')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][12]['entropy'], '3.05255')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][13]['entropy'], '5.46602')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][14]['entropy'], '3.2389')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][15]['entropy'], '3.41042')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][16]['entropy'], '3.11942')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][17]['entropy'], '4.4406')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][18]['entropy'], '1.06128')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][19]['entropy'], '1.06128')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][20]['entropy'], '1.41557')
         self.assertEqual(self.fileinfo.output['sectionTable']['sections'][21]['entropy'], '0')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][22]['entropy'], '0.0991956')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][23]['entropy'], '0.0835706')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][24]['entropy'], '0.3125')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][25]['entropy'], '0.470898')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][26]['entropy'], '0.210961')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][27]['entropy'], '0.59874')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][28]['entropy'], '0.532765')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][22]['entropy'], '0.793564')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][23]['entropy'], '0.668564')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][24]['entropy'], '2.5')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][25]['entropy'], '3.76718')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][26]['entropy'], '1.68769')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][27]['entropy'], '4.78992')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][28]['entropy'], '4.26212')
 
     def test_overlay_entropy(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output['overlay']['entropy'], '0.0586244')
+        self.assertEqual(self.fileinfo.output['overlay']['entropy'], '0.468996')
 
 class TestMACHO(Test):
     settings = TestSettings(
@@ -68,17 +68,17 @@ class TestMACHO(Test):
 
     def test_section_entropy(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][0]['entropy'], '0.464502')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][1]['entropy'], '0.281454')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][2]['entropy'], '0.327149')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][3]['entropy'], '0.377757')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][4]['entropy'], '0.190964')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][0]['entropy'], '3.71602')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][1]['entropy'], '2.25163')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][2]['entropy'], '2.61719')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][3]['entropy'], '3.02206')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][4]['entropy'], '1.52771')
         self.assertEqual(self.fileinfo.output['sectionTable']['sections'][5]['entropy'], '0')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][6]['entropy'], '0.1875')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][6]['entropy'], '1.5')
 
     def test_overlay_entropy(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output['overlay']['entropy'], '0.738361')
+        self.assertEqual(self.fileinfo.output['overlay']['entropy'], '5.90689')
 
 class TestCOFF(Test):
     settings = TestSettings(
@@ -89,11 +89,11 @@ class TestCOFF(Test):
 
     def test_section_entropy(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][0]['entropy'], '0.559017')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][3]['entropy'], '0.444582')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][4]['entropy'], '0.590768')
-        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][5]['entropy'], '0.43337')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][0]['entropy'], '4.47214')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][3]['entropy'], '3.55666')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][4]['entropy'], '4.72614')
+        self.assertEqual(self.fileinfo.output['sectionTable']['sections'][5]['entropy'], '3.46696')
 
     def test_overlay_entropy(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output['overlay']['entropy'], '0.458062')
+        self.assertEqual(self.fileinfo.output['overlay']['entropy'], '3.6645')
