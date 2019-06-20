9964dad1894fc7201c5cd3f80071efec52aea238
Zezula Ladislav <ladislav.zezula@avast.com>
2019-06-20 09:46:48 +0200

    A missing dependency must not be reported as loader error

--- tools/fileinfo/features/pe-missing-dependencies/test.py
+++ tools/fileinfo/features/pe-missing-dependencies/test.py
@@ -28,8 +28,7 @@ class Test001(Test):
 
     def test_missing_deps(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output["loaderError"]["code"], 42)
-        self.assertEqual(self.fileinfo.output["loaderError"]["code_text"], 'LDR_ERROR_MISSING_DEPENDENCY')
+        self.assertFalse("loaderError" in self.fileinfo.output)
         self.assertEqual(self.fileinfo.output["missingDeps"]["count"], '2')
         self.assertEqual(self.fileinfo.output["missingDeps"]["items"][0]["name"], "ADVAPY32.dll")
         self.assertEqual(self.fileinfo.output["missingDeps"]["items"][1]["name"], "KERNUL32.dll")
