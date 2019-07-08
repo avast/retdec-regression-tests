e342113fc722a8f91e464fd4531dae5379f6358c
Zezula Ladislav <ladislav.zezula@avast.com>
2019-07-08 13:11:04 +0200

    Whitespaces fixed

--- tools/fileinfo/features/pe-packers/test.py
+++ tools/fileinfo/features/pe-packers/test.py
@@ -40,8 +40,8 @@ class Test_SafeDisc(Test):
     def test_corrupted_pe(self):
         assert self.fileinfo.succeeded
         self.assertTrue((self.fileinfo.output['tools'][1]['name'] == 'SafeDisc') or
-		                (self.fileinfo.output['tools'][2]['name'] == 'SafeDisc') or
-						(self.fileinfo.output['tools'][3]['name'] == 'SafeDisc'))
+                        (self.fileinfo.output['tools'][2]['name'] == 'SafeDisc') or
+                        (self.fileinfo.output['tools'][3]['name'] == 'SafeDisc'))
 
 class Test_SecuROM(Test):
     settings=TestSettings(
