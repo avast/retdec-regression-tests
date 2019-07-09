7b97d0be8bd25b164765e60b64b8f8abc972e694
Peter Matula <peter.matula@avast.com>
2019-07-09 12:40:21 +0200

    tools/r2plugin/ack: add more tests.

--- tools/r2plugin/ack/test.py
+++ tools/r2plugin/ack/test.py
@@ -1,11 +1,21 @@
 from regression_tests import *
 
-class TestX86GccExe(Test):
+class TestX86GccExe_ack(Test):
     settings = TestSettings(
         tool='r2plugin',
         input='ack.x86.mingw32-gcc-4.7.3.O0.g.ex',
-        #args=''
+        args='--select 0x401560' # ack
     )
 
     def test(self):
-        print('===========> TestX86GccExe::test()')
+        assert self.out_c.has_just_funcs('ack')
+
+class TestX86GccExe_main(Test):
+    settings = TestSettings(
+        tool='r2plugin',
+        input='ack.x86.mingw32-gcc-4.7.3.O0.g.ex',
+        args='--select 0x4015bb' # main
+    )
+
+    def test(self):
+        assert self.out_c.has_just_funcs('main')
