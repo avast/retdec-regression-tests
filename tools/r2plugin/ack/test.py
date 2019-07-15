39f143aefe390355f0e6816cc4303d8845fc63c3
Peter Kubov <peter.kubov@avast.com>
2019-07-15 03:37:56 +0200

    tools/r2plugin/ack: add more tests

--- tools/r2plugin/ack/test.py
+++ tools/r2plugin/ack/test.py
@@ -1,5 +1,14 @@
 from regression_tests import *
 
+class CommonTest(Test):
+    settings = TestSettings(
+            tool='r2plugin',
+            input=files_in_dir('.', matching=r'ack.*')
+    )
+
+    def test_contains_main(self):
+        assert self.out_c.has_funcs('main') or self.out_c.has_funcs('_main')
+
 class TestX86GccExe_ack(Test):
     settings = TestSettings(
         tool='r2plugin',
@@ -7,7 +16,7 @@ class TestX86GccExe_ack(Test):
         args='--select 0x401560' # ack
     )
 
-    def test(self):
+    def test_for_ack(self):
         assert self.out_c.has_just_funcs('ack')
 
 class TestX86GccExe_main(Test):
@@ -17,5 +26,5 @@ class TestX86GccExe_main(Test):
         args='--select 0x4015bb' # main
     )
 
-    def test(self):
+    def test_for_main(self):
         assert self.out_c.has_just_funcs('main')
