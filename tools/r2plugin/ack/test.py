302d5d9bf3d97fd48ce9095b133eedecd5cd8c3b
Peter Kubov <peter.kubov@avast.com>
2019-08-28 20:37:30 +0200

    r2plugin: provide tests for project file

--- tools/r2plugin/ack/test.py
+++ tools/r2plugin/ack/test.py
@@ -1,29 +1,32 @@
 from regression_tests import *
 
-class CommonTest(Test):
-    settings = TestSettings(
-            tool='r2plugin',
-            input=files_in_dir('.', matching=r'ack.*')
-    )
-
-    def test_contains_main(self):
-        assert self.out_c.has_funcs('main') or self.out_c.has_funcs('_main')
-
 class TestX86GccExe_ack(Test):
     settings = TestSettings(
         tool='r2plugin',
-        input='ack.x86.mingw32-gcc-4.7.3.O0.g.ex',
-        args='--select 0x401560' # ack
+        input='ack.x86.clang.macho',
+        args='--select 0x01e90' # ack
     )
 
     def test_for_ack(self):
-        assert self.out_c.has_just_funcs('ack')
+        assert self.out_c.has_just_funcs('_ack')
+
+
+class TestProjFile(Test):
+    settings = TestSettings(
+        tool='r2plugin',
+        input='ack.x86.clang.macho',
+        project='ackproj/rc',
+        args='--select 0x01e90' # ack
+    )
+
+    def test_for_ack(self):
+        assert self.out_c.has_just_funcs('_ack')
 
 class TestX86GccExe_main(Test):
     settings = TestSettings(
         tool='r2plugin',
-        input='ack.x86.mingw32-gcc-4.7.3.O0.g.ex',
-        args='--select 0x4015bb' # main
+        input='ack.x86.clang.macho',
+        args='--select 0x1ee0' # main
     )
 
     def test_for_main(self):
