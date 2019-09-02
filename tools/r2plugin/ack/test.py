e70f86daf424b2c92d26f49fe29a59c4dbf820ab
Peter Kubov <peter.kubov@avast.com>
2019-09-02 18:52:52 +0200

    r2pluign: provide better functionality testing
    Currenty R2 stores computer-dependant information
    that is not suitable for computer-independent testing.
    
    This commit brings changes to testing - provides
    way to pass specific commands that will be executed
    as initial commands after R2 loads the input binary.
    
    As R2 projects may be improved in the future,
    the functionality to use project files will stay
    supported.

--- tools/r2plugin/ack/test.py
+++ tools/r2plugin/ack/test.py
@@ -4,22 +4,42 @@ class TestX86GccExe_ack(Test):
     settings = TestSettings(
         tool='r2plugin',
         input='ack.x86.clang.macho',
-        args='--select 0x01e90' # ack
+        args='--select 0x01e90'  # ack
     )
 
     def test_for_ack(self):
         assert self.out_c.has_just_funcs('_ack')
+
+class TestRenameFunction(Test):
+    settings = TestSettings(
+        tool='r2plugin',
+        input='ack.x86.clang.macho',
+        commands=(
+            's 0x01e90',  # Seek address
+            'afn ack_renamed'  # Rename function
+        ),
+        args='--select 0x01e90'  # ack
+    )
+
+    def test_ack_is_renamed(self):
+        assert self.out_c.has_just_funcs('ack_renamed')
 
 
 class TestProjFile(Test):
     settings = TestSettings(
         tool='r2plugin',
         input='ack.x86.clang.macho',
-        project='ackproj/rc',
+        commands=(
+            's 0x01e90',            # Seek address
+            'afvb-*',               # Remove all local vars
+            'afn ack',              # Rename function
+            'afvb 8 m int32_t',     # Provide stack var m
+            'afvb 12 n int32_t',    # Provide stack var n
+        ),
         args='--select 0x01e90' # ack
     )
 
-    def test_for_ack(self):
+    def test_parameters_are_set(self):
         assert self.out_c.has_just_funcs('ack')
         assert self.out_c.funcs['ack'].param_count == 2
         assert self.out_c.funcs['ack'].params[0].type.is_int(32)
