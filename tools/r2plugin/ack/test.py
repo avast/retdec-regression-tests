cb1f7a68160ef6e6927cde2b2fd06cc90fe50915
Peter Kubov <peter.kubov@avast.com>
2019-09-02 21:37:38 +0200

    tools/r2plugin: test export custom arguments

--- tools/r2plugin/ack/test.py
+++ tools/r2plugin/ack/test.py
@@ -25,7 +25,7 @@ class TestRenameFunction(Test):
         assert self.out_c.has_just_funcs('ack_renamed')
 
 
-class TestProjFile(Test):
+class TestArgs(Test):
     settings = TestSettings(
         tool='r2plugin',
         input='ack.x86.clang.macho',
@@ -46,6 +46,31 @@ class TestProjFile(Test):
         assert self.out_c.funcs['ack'].params[1].type.is_int(32)
         assert self.out_c.funcs['ack'].params[0].name == "m"
         assert self.out_c.funcs['ack'].params[1].name == "n"
+        assert self.out_c.funcs['ack'].calls('ack')
+        assert self.out_c.funcs['ack'].has_any_if_stmts()
+
+
+class TestArgsCustom(Test):
+    settings = TestSettings(
+        tool='r2plugin',
+        input='ack.x86.clang.macho',
+        commands=(
+            's 0x01e90',            # Seek address
+            'afvb-*',               # Remove all local vars
+            'afn ack',              # Rename function
+            'afvb 8 xxx int64_t',     # Provide stack var m
+            'afvb 12 yyy char',    # Provide stack var n
+        ),
+        args='--select 0x01e90' # ack
+    )
+
+    def test_parameters_are_set(self):
+        assert self.out_c.has_just_funcs('ack')
+        assert self.out_c.funcs['ack'].param_count == 2
+        assert self.out_c.funcs['ack'].params[0].type.is_int(64)
+        assert self.out_c.funcs['ack'].params[1].type.is_char()
+        assert self.out_c.funcs['ack'].params[0].name == "xxx"
+        assert self.out_c.funcs['ack'].params[1].name == "yyy"
         assert self.out_c.funcs['ack'].calls('ack')
         assert self.out_c.funcs['ack'].has_any_if_stmts()
 
