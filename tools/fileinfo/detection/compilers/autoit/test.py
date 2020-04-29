e1d11dbab37531ceef5cff88b68e072e3054cc82
Piotr Szczepanski <piotr.szczepanski@avast.com>
2020-04-29 13:21:40 +0200

    Add tests for improvements to AutoIT detection.
    The improvements allow the detection of a wider range of versions of files
    compiled with Aut2Exe. In addition, this commit improves the
    implementation of currently existing tests so that they can also test
    new cases.

--- tools/fileinfo/detection/compilers/autoit/test.py
+++ tools/fileinfo/detection/compilers/autoit/test.py
@@ -3,10 +3,15 @@ from regression_tests import *
 class TestAutoItDetection(Test):
     settings = TestSettings(
         tool='fileinfo',
-        input=files_in_dir('inputs')
+        input=files_in_dir('inputs'),
+        args='--json'
     )
 
     def test_detected_autoit(self):
-        assert self.fileinfo.succeeded
-        assert self.fileinfo.output.contains(r'.*Aut2Exe \(3\.3\..*\)')
-        assert self.fileinfo.output.contains(r'.*AutoIt \(bytecode\)')
+        autoit_recognised = False
+
+        self.assertTrue(self.fileinfo.succeeded)
+        for tool in self.fileinfo.output["tools"]:
+            if tool['type'] == 'compiler' and tool['name'] == 'Aut2Exe':
+                autoit_recognised = True
+        self.assertTrue(autoit_recognised)
