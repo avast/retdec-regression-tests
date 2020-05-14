1c2661692a4a3d4763c7bd567ed2f41b871d386a
Peter Matula <peter.matula@avast.com>
2020-05-14 20:28:00 +0200

    features/bytecode-detection: do not test fileinfo in decompiler tests

--- features/bytecode-detection/test.py
+++ features/bytecode-detection/test.py
@@ -16,9 +16,6 @@ class Test(Test):
     def setUp(self):
         pass
 
-    def test_fileinfo_detects_bytecode(self):
-        self.assertEqual(self.decompiler.fileinfo_output['Original language'], 'CIL/.NET (bytecode)')
-
     def test_warning_is_emitted(self):
         # Do NOT change the format of the following warning because the
         # decompilation service depends on it.
