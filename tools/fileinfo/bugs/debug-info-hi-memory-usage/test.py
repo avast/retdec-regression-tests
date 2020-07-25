aeaaec39b0b03891b82b30c0b8d6581ddfb87993
Ladislav Zezula <ladislav.zezula@avast.com>
2020-07-25 19:04:06 +0200

    Added regression tests for low memory usage on corrupt debug directory

---
+++ tools/fileinfo/bugs/debug-info-hi-memory-usage/test.py
@@ -0,0 +1,13 @@
+from regression_tests import *
+
+class Test(Test):
+    settings=TestSettings(
+        tool='fileinfo',
+        args='--json --verbose',
+        input=[
+            '022afb79c539b23aecfad315c7d9a712d747b521952184446026f7f2aa9005b8',
+        ]
+    )
+
+    def test_output_contains_dotnet_info(self):
+        assert self.fileinfo.succeeded
