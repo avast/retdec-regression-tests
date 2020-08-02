1e60ab6115248b58343802613b8de2507772c2e1
Peter Kubov <peter.kubov@avast.com>
2020-08-02 13:02:46 +0200

    unreachable-fncs-in-object-files: enable test

--- bugs/unreachable-fncs-in-object-files/test.py
+++ bugs/unreachable-fncs-in-object-files/test.py
@@ -1,16 +1,16 @@
 from regression_tests import *
 
-#class Test(Test):
-    #settings = TestSettings(
-        #input='module.o'
-    #)
+class Test(Test):
+    settings = TestSettings(
+        input='module.o'
+    )
 
-    #def test_function_present(self):
-        #assert self.out_c.has_funcs('func', 'main')
+    def test_function_present(self):
+        assert self.out_c.has_funcs('func', 'main')
         #assert self.out_c.has_comment_matching('// Address range: 0x0 - 0x1c')
         #assert self.out_c.has_comment_matching('// Address range: 0x1d - 0x26')
 
-    #def test_function_func(self):
-        #func = self.out_c.funcs['func']
-        #assert func.calls('printf')
-        #assert self.out_c.has_string_literal('Hellow world from func()')
+    def test_function_func(self):
+        func = self.out_c.funcs['func']
+        assert func.calls('printf')
+        assert self.out_c.has_string_literal('Hellow world from func()')
