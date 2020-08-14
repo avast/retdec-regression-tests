2909a01d2023edb891bf9ae01a6d470dab09bdd5
Petr Zemek <petr.zemek@avast.com>
2020-08-14 13:49:49 +0200

    Move a test from #69 into a better directory.
    There is already a directory for fileinfo crashes, so move the test in there.
    Otherwise, we would have two directories for the same purpose.

--- tools/fileinfo/bugs/fileinfo-crashes/test.py
+++ tools/fileinfo/bugs/fileinfo-crashes/test.py
@@ -1,6 +1,6 @@
 from regression_tests import *
 
-class Test(Test):
+class TestValidPEWithoutCrash(Test):
     settings = TestSettings(
         tool='fileinfo',
         input='x86-pe-036ff6f43fac0491dd827c4fb508cd4b',
@@ -10,3 +10,16 @@ class Test(Test):
     def test_fileinfo_succeeded(self):
         assert self.fileinfo.succeeded
         self.assertEqual(self.fileinfo.output['fileFormat'], 'PE')
+
+class TestCorruptedPEWithoutCrash(Test):
+    settings=TestSettings(
+        tool='fileinfo',
+        input=[
+            # https://github.com/avast/retdec/issues/838
+            '447360831D2F44DA6CB38D49812DA0921F18C37052FBBA424DE19290563250F8',
+        ],
+        args='--json'
+    )
+
+    def test_corrupted_pe(self):
+        assert 'errors' in self.fileinfo.output
