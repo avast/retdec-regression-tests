7bea0fbb312922e434ba54f3020ba141323efc58
Ladislav Zezula <ladislav.zezula@avast.com>
2020-09-23 15:48:22 +0200

    Added YARA rule for missing NSIS installers

--- tools/fileinfo/detection/installers/nsis/test.py
+++ tools/fileinfo/detection/installers/nsis/test.py
@@ -1,5 +1,19 @@
 from regression_tests import *
 
+class TestNSISDetectionByOverlay(Test):
+    settings = TestSettings(
+        tool='fileinfo',
+        input=[
+            'nsis10f.exe',
+            'nsis11n.exe',
+            'nsis142.exe'
+        ],
+    )
+
+    def test_detected_nsis(self):
+        assert self.fileinfo.succeeded
+        assert self.fileinfo.output.contains(r'Nullsoft Install System')
+
 class TestNSISDetection(Test):
     settings = TestSettings(
         tool='fileinfo',
