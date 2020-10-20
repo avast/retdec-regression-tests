9a1ffd3de4cdf9fc3b468febcfe7db325c483126
Ladislav Zezula <ladislav.zezula@avast.com>
2020-10-20 16:14:42 +0200

    Fixed issue #872

--- tools/fileinfo/features/max-memory/test.py
+++ tools/fileinfo/features/max-memory/test.py
@@ -33,16 +33,17 @@ class TestAnalysisWhenSufficient(Test):
             '--max-memory=104857600',  # 100 MB
             '--max-memory-half-ram',
         ],
-        input='ack.ex',
+        input=[
+            'ack.ex',
+            'E556E9A67BBED5B6C32BE434A148841FEFEE81EB39432D427CB08FEFB0098173'
+        ]
     )
 
     def test_correctly_analyzes_file(self):
         assert self.fileinfo.succeeded
 
-        self.assertEqual(
-            self.fileinfo.output['SHA256'],
-            '35f7b82373b66579d782fa61732da86717631eff95876b799a9549661709fd8b'
-        )
+        assert(self.fileinfo.output['SHA256'] == '35f7b82373b66579d782fa61732da86717631eff95876b799a9549661709fd8b' or \
+               self.fileinfo.output['SHA256'] == 'e556e9a67bbed5b6c32be434a148841fefee81eb39432d427cb08fefb0098173')
 
 # Memory limiting does not work correctly on macOS (see
 # https://github.com/avast/retdec/issues/379).
