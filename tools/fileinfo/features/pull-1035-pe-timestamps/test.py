5c6c3c4246fca1b975a2933594ca1169b8e28344
houndthe <cgkajm@gmail.com>
2021-10-19 17:15:56 +0200

    Update timestamp format

--- tools/fileinfo/features/pull-1035-pe-timestamps/test.py
+++ tools/fileinfo/features/pull-1035-pe-timestamps/test.py
@@ -11,20 +11,20 @@ class Test1(Test):
     def test_pe_timestamps(self):
         assert self.fileinfo.succeeded
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['coffHeader'], '2016-01-09T04:52:11+0000')
+                         ['coffHeader'], 'Jan  9 04:52:11 2016 GMT')
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['loadConfigDir'], '2031-04-18T05:10:52+0000')
+                         ['loadConfigDir'], 'Apr 18 05:10:52 2031 GMT')
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['exportDir'], "2021-08-23T03:12:51+0000")
+                         ['exportDir'], "Aug 23 03:12:51 2021 GMT")
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['debugDirEntries'][0], "2016-01-09T04:52:11+0000")
+                         ['debugDirEntries'][0], "Jan  9 04:52:11 2016 GMT")
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['debugDirEntries'][1], "2016-01-09T04:52:11+0000")
+                         ['debugDirEntries'][1], "Jan  9 04:52:11 2016 GMT")
         self.assertEqual(
             self.fileinfo.output['timestamps']['resourceDirectories'][0],
-            '2021-10-19T11:13:06+0000')
+            'Oct 19 11:13:06 2021 GMT')
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['pdbDebugInfos'][0], '2006-07-25T04:53:49+0000')
+                         ['pdbDebugInfos'][0], 'Jul 25 04:53:49 2006 GMT')
 
 
 class Test2(Test):
@@ -37,18 +37,18 @@ class Test2(Test):
     def test_pe_timestamps(self):
         assert self.fileinfo.succeeded
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['coffHeader'], '2016-04-06T14:39:04+0000')
+                         ['coffHeader'], 'Apr  6 14:39:04 2016 GMT')
         self.assertEqual(self.fileinfo.output['timestamps']
-                         ['loadConfigDir'], '1970-01-01T00:02:52+0000')
+                         ['loadConfigDir'], 'Jan  1 00:02:52 1970 GMT')
         self.assertEqual(
             self.fileinfo.output['timestamps']['resourceDirectories'][0],
-            '1970-08-09T18:46:24+0000')
+            'Aug  9 18:46:24 1970 GMT')
         self.assertEqual(
             self.fileinfo.output['timestamps']['resourceDirectories'][1],
-            '1970-02-07T06:22:42+0000')
+            'Feb  7 06:22:42 1970 GMT')
         self.assertEqual(
             self.fileinfo.output['timestamps']['resourceDirectories'][2],
-            '1971-08-21T02:23:31+0000')
+            'Aug 21 02:23:31 1971 GMT')
         self.assertEqual(
             self.fileinfo.output['timestamps']['resourceDirectories'][3],
-            "2024-04-10T04:14:11+0000")
+            "Apr 10 04:14:11 2024 GMT")
