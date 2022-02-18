cb21b64d023ade69a3e602b60f159757921ca940
Petr Zemek <petr.zemek@avast.com>
2022-02-18 08:15:10 +0100

    Remove useless trailing whitespace
    There is absolutely no reason for it being in the code.

--- features/output-format/test.py
+++ features/output-format/test.py
@@ -5,7 +5,7 @@ class Test_plain(Test):
         input='ack.x64.gcc.O0.g.exe',
         args='--output-format=plain'
     )
-    
+
     def test_produce_expected_output(self):
         assert self.out_c.contains('int main\(int argc, char \*\* argv\)')
 
@@ -14,7 +14,7 @@ class Test_json(Test):
         input='ack.x64.gcc.O0.g.exe',
         args='--output-format=json'
     )
-    
+
     def test_produce_expected_output(self):
         assert self.out_c.contains('{"kind":"i_fnc","val":"main"}')
 
@@ -23,7 +23,7 @@ class Test_json_human(Test):
         input='ack.x64.gcc.O0.g.exe',
         args='--output-format=json-human'
     )
-    
+
     def test_produce_expected_output(self):
         assert self.out_c.contains('"kind": "i_fnc"')
         assert self.out_c.contains('"val": "main"')
@@ -33,12 +33,12 @@ class Test_json_bad(Test):
         input='ack.x64.gcc.O0.g.exe',
         args='--output-format=bad_format'
     )
-    
+
     def setUp(self):
         # Prevent the base class from checking that the decompilation
         # succeeded (we expect it to fail).
         pass
-    
+
     def test_decompilation_fails_with_correct_error_message(self):
         self.assertNotEqual(self.decompiler.return_code, 0)
         assert self.decompiler.log.contains('Error: \[-f|--output-format\] unknown output format: bad_format')
