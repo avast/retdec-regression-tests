cb613c6d8bc635b8a34c43925a5c607aa6c2cf83
Ladislav Zezula <ladislav.zezula@avast.com>
2022-11-02 19:19:17 +0100

    Fixed error code

--- tools/fileinfo/features/section-virtual-size-too-big/test.py
+++ tools/fileinfo/features/section-virtual-size-too-big/test.py
@@ -7,13 +7,13 @@ class Test1(Test):
         tool='fileinfo',
         input=[
             '83e9cb2a6e78c742e1090292acf3c78baf76e82950d96548673795a1901db061',
-            'a882bc57d078511c0fb15ea956d5655a626b508c91c4dc6d7a3ba467fc396b71'
-        ]
+            'a882bc57d078511c0fb15ea956d5655a626b508c91c4dc6d7a3ba467fc396b71',
+        ],
         args='--verbose --json'
     )
 
     def test_loader_survives(self):
         assert self.fileinfo.succeeded
-        self.assertEqual(self.fileinfo.output["loaderError"]["code"], 29)
-        self.assertEqual(self.fileinfo.output["loaderError"]["code_text"], 'LDR_ERROR_INVALID_SECTION_VA')
+        self.assertEqual(self.fileinfo.output["loaderError"]["code"], 30)
+        self.assertEqual(self.fileinfo.output["loaderError"]["code_text"], 'LDR_ERROR_INVALID_SECTION_VSIZE')
         self.assertEqual(self.fileinfo.output["loaderError"]["loadable_anyway"], 'false')
