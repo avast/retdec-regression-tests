a17c096163a7cfdaabc36dfd053745bbac95cb78
Ladislav Zezula Ladislav.Zezula@gendigital.com <ladislav.zezula@gendigital.com>
2023-04-20 15:24:21 +0200

    Test class renamed

--- tools/fileinfo/detection/installers/ms_setup/test.py
+++ tools/fileinfo/detection/installers/ms_setup/test.py
@@ -1,11 +1,11 @@
 from regression_tests import *
 
-class TestWixDetection(Test):
+class TestMsSetupDetection(Test):
     settings = TestSettings(
         tool='fileinfo',
         input=files_in_dir('inputs')
     )
 
-    def test_detected_wix(self):
+    def test_detected_ms_setup(self):
         assert self.fileinfo.succeeded
         assert self.fileinfo.output.contains(r'.*Microsoft Setup \(8\..*\)')
